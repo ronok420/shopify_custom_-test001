fac8b1ee12d7a29ded536caec62690fbdb58b36a
ronok420 <shahariarronok41.com>
2024-03-19 12:33:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/*.liquid",
+    "./assets/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/customers/*.liquid",
+    "./templates/*.liquid"
+
+
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
